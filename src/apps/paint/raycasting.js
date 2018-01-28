1ecb0a1158947ccb2d435153c88a0c16853bb19b
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2018-01-28 14:40:25 +0100

    raycasting fix

--- src/apps/paint/raycasting.js
+++ src/apps/paint/raycasting.js
@@ -1,6 +1,6 @@
 const intersects = (p, p1, p2) => {
   if (p[0] > p1[0] || p[0] > p2[0])
-    return (p[1] >= p1[1] && p[1] < p2[1]) || (p[1] <= p1[1] && p[1] > p2[1]);
+    return (p[1] >= p1[1] && p[1] < p2[1]) || (p[1] < p1[1] && p[1] >= p2[1]);
   else
     return false;
 }
@@ -9,12 +9,10 @@ const intersects = (p, p1, p2) => {
 export const pointWithinShape = (point, path) => {
   var count = 0;
   var p1, p2;
-  path = [...path, path[0]]
   for (var i = 0; i < path.length; i++) {
     p1 = path[i];
     p2 = path[(i + 1) % path.length];
     if (intersects(point, p1, p2)) {
-      console.log('intersects!')
       count++;
     }
   }
