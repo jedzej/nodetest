b2ceac82f0b4a558d20b36968117e1529aa8e795
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2018-02-23 16:29:27 +0100

    bottom navigation added

--- src/apps/avaclone/containers/StageWrapper.jsx
+++ src/apps/avaclone/containers/StageWrapper.jsx
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import withStyles from 'material-ui/styles/withStyles';
 import Paper from 'material-ui/Paper/Paper';
 import Divider from 'material-ui/Divider';
+import People from 'material-ui-icons/People'
+import Settings from 'material-ui-icons/Settings'
+import GpsNotFixed from 'material-ui-icons/GpsNotFixed'
+import BottomNavigation, { BottomNavigationAction } from 'material-ui/BottomNavigation';
 
 import ActionTip from './common/ActionTip';
 import ProgressTable from './progress/ProgressTable';
@@ -17,7 +21,6 @@ const styles = theme => ({
   paper: {
     padding: theme.spacing.unit * 2,
     margin: theme.spacing.unit * 2,
-    height: '100%',
   },
   divider: {
     marginTop: theme.spacing.unit,
@@ -28,31 +31,54 @@ const styles = theme => ({
 
 class StageWrapper extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: 'quest'
+    };
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ tab: value });
+  }
+
   render() {
     const { classes } = this.props;
     const { store } = this.props.avaclone;
     const quest = ac.get.currentQuest(store);
+    const { tab } = this.state;
 
     return (
       <div style={{ margin: 0, height: '100%' }}>
-        <div style={{ margin: 0, overflow: 'auto' }}>
-          {quest &&
+        <div style={{ margin: 0, overflow: 'auto', height: 'calc(100% - 56px)' }}>
+          {quest && tab === 'quest' &&
             <Paper className={classes.paper}>
               <QuestInfo strong align="center" questNumber={quest.number} />
             </Paper>
           }
-          <Paper className={classes.paper}>
-            <ActionTip />
-            <Divider className={classes.divider} />
-            {this.props.children}
-          </Paper>
-          <Paper className={classes.paper}>
-            <ProgressTable />
-          </Paper>
-          <Paper className={classes.paper}>
-            <CharactersPanel />
-          </Paper>
+          {tab === 'quest' &&
+            <Paper className={classes.paper}>
+              <ProgressTable />
+            </Paper>
+          }
+          {tab === 'quest' &&
+            <Paper className={classes.paper}>
+              <ActionTip />
+              <Divider className={classes.divider} />
+              {this.props.children}
+            </Paper>
+          }
+          {tab === 'squad' &&
+            < Paper className={classes.paper}>
+              <CharactersPanel />
+            </Paper>
+          }
         </div>
+        <BottomNavigation value={tab} onChange={this.handleChange} className={classes.root}>
+          <BottomNavigationAction label="quest" value="quest" icon={<GpsNotFixed />} />
+          <BottomNavigationAction label="squad" value="squad" icon={<People />} />
+          <BottomNavigationAction label="settings" value="settings" icon={<Settings />} />
+        </BottomNavigation>
       </div>
     );
   }
